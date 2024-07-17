e9b35a1b50204059cae26e430fa7d339a0a53c0e
Massimo De Agro <massimo.deagro@gmail.com>
2024-07-17 11:23:22 +0200

    Create preview.py
    added preview file

---
+++ preview.py
@@ -0,0 +1,13 @@
+from picamera2 import Picamera2
+import time
+
+# adding here static variables for ease of use
+res = (1280, 1024)
+
+# main
+picam2 = Picamera2()
+picam2.preview_configuration.size = res
+
+picam2.start_preview()
+time.sleep(10)
+picam2.close()
