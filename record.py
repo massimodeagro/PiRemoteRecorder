d41f94bcdaf1e8054b324fa6d1d1dba39d4cdc71
Massimo De Agro <massimo.deagro@gmail.com>
2024-07-17 11:58:52 +0200

    Update record.py

--- record.py
+++ record.py
@@ -4,7 +4,6 @@ from libcamera import controls
 # adding here static variables for ease of use
 fps = 60
 res = (1280, 1024)
-lens_position = 0.5
 
 # here you have user specified data
 file_name = input('please insert the video name: ')
@@ -20,7 +19,6 @@ recording_duration = (int(recording_duration_h)*60 + int(recording_duration_m)) 
 picam2 = Picamera2()
 picam2.video_configuration.controls.FrameRate = fps
 picam2.preview_configuration.size = res
-picam2.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": lens_position})
 
 picam2.start_and_record_video(file_name+'.mp4', duration=recording_duration)
 picam2.close()
