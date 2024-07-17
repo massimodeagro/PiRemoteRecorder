860d45ddd7ff8250a7592813471365cf1541feff
Massimo De Agro <massimo.deagro@gmail.com>
2024-07-17 11:55:53 +0200

    Update preview.py

--- preview.py
+++ preview.py
@@ -1,12 +1,15 @@
 from picamera2 import Picamera2, Preview
 import time
 
+lens_position = float(input('specify lens position from 0 to 1'))
+                      
 # adding here static variables for ease of use
 res = (1280, 1024)
 
 # main
 picam2 = Picamera2()
 picam2.preview_configuration.size = res
+picam2.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": lens_position})
 
 picam2.start(show_preview=True)
 time.sleep(10)
